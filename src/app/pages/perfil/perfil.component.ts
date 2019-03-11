f5c4cfe2b924a128022f87733db39566daec8cf0
Maximiliano Minetto <maximinetto@gmail.com>
2019-03-11 17:29:42 -0300

    Se muestran los roles de cada usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,3 +1,5 @@
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { TOKEN_NAME } from './../../_shared/var.constants';
 import { Rol } from './../../_model/rol';
 import { Component, OnInit } from '@angular/core';
 
@@ -13,13 +15,33 @@ export class PerfilComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    let rol: Rol = new Rol();
+
+    this.listar();
+
+    /*let rol: Rol = new Rol();
     rol.idRol = 1;
     rol.nombre = 'Administrador';
     let rol2: Rol = new Rol();
     rol2.idRol = 2;
     rol2.nombre = 'BD';
-    this.roles.push(rol,rol2);
+    this.roles.push(rol,rol2);*/
+
+
   }
 
+  listar(){
+
+    const helper = new JwtHelperService();
+    let access_token = JSON.parse(sessionStorage.getItem(TOKEN_NAME)).access_token;
+
+    const decodedToken = helper.decodeToken(access_token);
+    const largo = decodedToken.authorities.length;
+    for(let i=0; i<largo; i++){
+      let rolName = decodedToken.authorities[i];
+      let rol : Rol = new Rol();
+      rol.nombre = rolName;
+      this.roles.push(rol);
+    }
+      
+  }
 }
